58a6b3558a5e86a251a0294d5e53ac91a92b551a
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-04-13 18:08:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this._name = name;
+		this._age = age;
+	}
 
-class Student extends Person {}
+	get getName(){
+		return this._name;
+	}
 
-class Teacher extends Person {}
+	set setAge(age){
+		this._age = age;
+	}
+}
+
+class Student extends Person {
+	super(name);
+	study(){
+		console.log(this._name + " is studying");
+	}
+}
+
+class Teacher extends Person {
+	super(name);
+	teach(){
+		console.log(this._name + " is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
